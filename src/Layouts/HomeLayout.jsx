359b3991e8786bcd5318b3a73a04ae9a201690be
anto_das <antuvhai@gmail.com>
2024-12-15 15:49:19 +0600

    project-layouts-setup

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold text-center">Home lay out</h2>
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import HomeLayout from './Layouts/HomeLayout';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <HomeLayout></HomeLayout>,
   },
+
+
+  
 ]);
 
 createRoot(document.getElementById('root')).render(
