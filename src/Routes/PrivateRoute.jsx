2628d8e3d0c2eb6d0e84766c9f98bdd2452c4920
anto_das <antuvhai@gmail.com>
2024-12-23 00:29:51 +0600

    some features added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
 import Loading from '../Components/Loading';
 import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const PrivateRoute = ({children}) => {
     const {user,loading} = useContext(AuthContext);
